99718ba7609266b5f6aea02099e660e6e53e9b6e
Adhwaith A M <adhwaith43@gmail.com>
2026-03-02 12:25:13 +0530

    react learning

--- reactworkssep/mypro/src/App.jsx
+++ reactworkssep/mypro/src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Welcome from './components/Welcome'
+import { Component } from 'react'
+import Component2 from './components/Component2'
+import Component1 from './components/Component1'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,25 +13,16 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Welcome/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <div>
+        <Component1/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        <Component2/>
+      </div>
     </>
   )
 }
