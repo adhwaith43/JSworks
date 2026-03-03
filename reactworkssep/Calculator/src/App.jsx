b7108270cb3cde3eefe9a996ef1e7612a15af063
Adhwaith A M <adhwaith43@gmail.com>
2026-03-03 18:10:49 +0530

    231b3b1o3

--- reactworkssep/Calculator/src/App.jsx
+++ reactworkssep/Calculator/src/App.jsx
@@ -3,24 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [result, setCount] = useState(0)
-  function add() {    setCount(result + 1)
+function App() 
+{
+
+  const[num1,setnum1]=useState('')
+  const[num2,setnum2]=useState('')
+  const[num3,setnum3]=useState('')
+
+  function add(){
+    console.log('add')
   }
-  function subtract() {    setCount(result - 1)
+  function sub(){
+    console.log('add')
   }
-  function multiply() {    setCount(result * 2)
+  function mul(){
+    console.log('add')
   }
-  function divide() {    setCount(result / 2)
+  function division(){
+    console.log('add')
   }
 
   return (
     <>
       <div className="container mt-5 w-25 p-5 border border-2 rounded">
         <h1 className='text-center'>Arithmetic Calculator</h1>
-        <input type="text" className="mt-3 form-control" value=""/>
+        <input type="text" className="mt-3 form-control" value="" placeholder="Enter a number"/>
         <br />
-        <input type="text" className="mt-3 form-control" value="" />
+        <input type="text" className="mt-3 form-control" value="" placeholder="Enter another number"/>
         <div className="mt-5 d-flex justify-content-between">
         <button className="btn btn-dark" onClick={add}>+</button>
         <button className="btn btn-primary" onClick={subtract}>-</button>
@@ -33,6 +42,7 @@ function App() {
       </div>
     </>
   )
+  
 }
 
 export default App
