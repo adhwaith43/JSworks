21f11088f39e4d06d22e0c3551ea541975e04649
Adhwaith A M <adhwaith43@gmail.com>
2026-03-03 16:26:29 +0530

    Finished the design phase

--- reactworkssep/Operations/src/components/Counter.jsx
+++ reactworkssep/Operations/src/components/Counter.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 function Counter() {
   return (
-    <div> 
-        <h3>Counter App</h3>
-        <input type="text" name="" id="" />
+    <div className='container mt-5 p-5 border border-secondary'> 
+        <h3 className="text-center">Counter App</h3>
+        
+        <input type="text" name="" id="" className="form-control"/>
         <br />
-        <button >+</button>
-        <button>-</button>
-        <button>Reset</button>
+        <div className="d-flex justify-content-evenly gap-3">
+        <button className="btn btn-primary">+</button>
+        <button className="btn btn-danger">-</button>
+        <button className="btn btn-secondary">Reset</button>
+        </div>
     </div>
   )
 }
--- reactworkssep/Operations/src/components/Home.jsx
+++ reactworkssep/Operations/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-        <h1>Welcome to My app</h1>
+        <h1 className="text-center">Welcome to My app</h1>
     </div>
   )
 }
