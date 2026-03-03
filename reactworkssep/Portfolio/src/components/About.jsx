ecec46b3a7d6eca9c56af08d36630988afabf1e1
Adhwaith A M <adhwaith43@gmail.com>
2026-03-03 11:14:44 +0530

    added: reactworkssep/Portfolio/src/App.jsx

--- reactworkssep/Portfolio/src/components/About.jsx
+++ reactworkssep/Portfolio/src/components/About.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <h1 class="text-center mt-5">Full Stack Developer</h1>
+      <p class="text-center mt-3">I am a full stack developer 
+        with experience in building web applications using React,
+         Node.js, and MongoDB. I have a strong background in JavaScript 
+         and I am passionate about learning new technologies and improving 
+         my skills.</p>
+      <div class="container-fluid bg-light mt-5 p-5">
+        <div class="col-6">
+                <div> Name : Milan</div>
+                <div> Age : 30</div>
+                <div> Location : New York</div>
+        </div>
+        <div class="col-6">
+           <div> Email : milan@example.com</div>
+           <div>Gender= Male</div>
+           <div>Phone: 123-456-7890</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
