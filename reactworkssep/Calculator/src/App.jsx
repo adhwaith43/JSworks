5d71415cdda42d4318cd45014eaedf74b65a0702
Adhwaith A M <adhwaith43@gmail.com>
2026-03-03 18:05:40 +0530

    hhiu1h2io3h1o3h

--- reactworkssep/Calculator/src/App.jsx
+++ reactworkssep/Calculator/src/App.jsx
@@ -4,30 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setCount] = useState(0)
+  function add() {    setCount(result + 1)
+  }
+  function subtract() {    setCount(result - 1)
+  }
+  function multiply() {    setCount(result * 2)
+  }
+  function divide() {    setCount(result / 2)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container mt-5 w-25 p-5 border border-2 rounded">
+        <h1 className='text-center'>Arithmetic Calculator</h1>
+        <input type="text" className="mt-3 form-control" value=""/>
+        <br />
+        <input type="text" className="mt-3 form-control" value="" />
+        <div className="mt-5 d-flex justify-content-between">
+        <button className="btn btn-dark" onClick={add}>+</button>
+        <button className="btn btn-primary" onClick={subtract}>-</button>
+        <button className="btn btn-secondary" onClick={multiply}>*</button>
+        <button className="btn btn-danger" onClick={divide}>/</button>
+        </div>
+        <div>
+          <h4 className="mt-3 text-center">Result: {result}</h4>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
