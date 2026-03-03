9f9a9292a3fae5f73dd0417f3092224565d4006c
Adhwaith A M <adhwaith43@gmail.com>
2026-03-03 16:29:38 +0530

    small corrections to the counter app and home page

--- reactworkssep/Operations/src/components/Counter.jsx
+++ reactworkssep/Operations/src/components/Counter.jsx
@@ -5,7 +5,7 @@ function Counter() {
     <div className='container mt-5 p-5 border border-secondary'> 
         <h3 className="text-center">Counter App</h3>
         
-        <input type="text" name="" id="" className="form-control"/>
+        <input type="text" name="" id="" className="form-control" value="0"/>
         <br />
         <div className="d-flex justify-content-evenly gap-3">
         <button className="btn btn-primary">+</button>
