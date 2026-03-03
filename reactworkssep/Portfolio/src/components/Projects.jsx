82733fd92db43d72baa51909d1c998e2889fbf5a
Adhwaith A M <adhwaith43@gmail.com>
2026-03-03 13:13:34 +0530

    usig map functions

--- reactworkssep/Portfolio/src/components/Projects.jsx
+++ reactworkssep/Portfolio/src/components/Projects.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 
 function Projects() {
+  let p=['Ecommerce Website','Task Management App','Weather Forecast App']
   return (
     (
     <div class="container-fluid bg-light mt-5 p-5">
       <h4 class="text-center">Projects</h4>
-      <ul class="mt-3">
-        <li>Ecommerce Website</li>
-        <li>Task Management App</li>
-        <li>Weather Forecast App</li>
-      </ul>
+      <ol class="mt-3">
+        {p.map((i)=><li>{i}</li>)}
+      </ol>
     </div>
   )
   )
--- reactworkssep/Portfolio/src/components/Skills.jsx
+++ reactworkssep/Portfolio/src/components/Skills.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 function Skills() {
+  let s=['Front End Development','Back End Development','Database Management']
   return (
     <div class="container-fluid bg-light mt-5 p-5">
       <h4 class="text-center">Skills</h4>
       <ul class="mt-3">
-        <li>Front End Development</li>
-        <li>Back End Development</li>
-        <li>Database Management</li>
+
+        {/* syntax of map fn - array.map(arrow function) */}
+        {s.map((i)=><li>{i}</li>)}
+
       </ul>
     </div>
   )
