590171890388ceead92ffd6e93861751c7de650a
Adhwaith A M <adhwaith43@gmail.com>
2026-03-03 15:33:26 +0530

    323131B23B12B

---
+++ reactworkssep/Operations/src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Counter() {
+  return (
+    <div> 
+        <h3>Counter App</h3>
+        <input type="text" name="" id="" />
+        <br />
+        <button >+</button>
+        <button>-</button>
+        <button>Reset</button>
+    </div>
+  )
+}
+
+export default Counter
