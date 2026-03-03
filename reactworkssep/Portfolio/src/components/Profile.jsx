68293eaa95560aaa48347b6df4d470f1b97c5517
Adhwaith A M <adhwaith43@gmail.com>
2026-03-03 11:00:37 +0530

    added download.jpeg and updated Profile.jsx and index.css

--- reactworkssep/Portfolio/src/components/Profile.jsx
+++ reactworkssep/Portfolio/src/components/Profile.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import image from '../assets/download.jpeg'
 function Profile() {
   return (
     <div class="container-fluid bg-light mt-2 p-5">
       <div class="row">
-        <div class="col-6">COL 1</div>
-        <div class="col-6">COL 2</div>
+        <div class="col-6 d-flex justify-content-center align-items-center">Hello my name is Milan</div>
+        <div class="col-6 d-flex justify-content-center align-items-center"><img src={image} alt="Profile" /></div>
       </div>
     </div>
   )
