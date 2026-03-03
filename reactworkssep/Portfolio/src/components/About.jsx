3d5b533accb2e3a77d3c7ad668905c5af32c7753
Adhwaith A M <adhwaith43@gmail.com>
2026-03-03 12:16:47 +0530

    using js object

--- reactworkssep/Portfolio/src/components/About.jsx
+++ reactworkssep/Portfolio/src/components/About.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 
 function About() {
+  let user={
+    name:"Milan",
+    age:30,
+    location:"New York",
+    email:"milan@example.com",
+    gender:'Male',
+    phone:'123-456-7890'
+  }   //javascript object
+
   return (
     <div>
       <div class="container-fluid bg-light mt-5 p-5">
         <h4 class="text-center mt-5">Full Stack Developer</h4>
         <div class="row mt-5">
               <div class="col-6 ">
-                      <div> Name : Milan</div>
-                      <div> Age : 30</div>
-                      <div> Location : New York</div>
+                      <div> Name : {user.name}</div>
+                      <div> Age : {user.age}</div>
+                      <div> Location : {user.location}</div>
               </div>
               <div class="col-6 ps-md-4">
-                <div> Email : milan@example.com</div>
-                <div>Gender : Male</div>
-                <div>Phone : 123-456-7890</div>
+                <div> Email : {user.email}</div>
+                <div>Gender : {user.gender}</div>
+                <div>Phone : {user["phone"]}</div>
               </div>
         </div>
       </div>
